44f5a726d2e8551b6f16dbb21fa68c99bf62bdc2
Valdimir Barros de Carvalho <valdimirbarrosti@gmail.com>
2024-04-13 20:45:10 -0300

    laravel 11 directory structure ok

---
+++ laravel.com/11.x/example-app/app/Listeners/ListenersExample.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class ListenersExample
+
+
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(object $event): void
+    {
+        //
+    }
+}
