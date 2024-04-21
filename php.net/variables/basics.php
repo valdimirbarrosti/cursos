327f00239b4ce02117fea0c0c42719740efc8766
Valdimir Barros de Carvalho <valdimirbarrosti@gmail.com>
2024-04-21 05:18:38 -0300

    commit

--- php.net/variables/basics.php
+++ php.net/variables/basics.php
@@ -7,3 +7,24 @@ echo "$var, $Var";      // outputs "Bob, Joe"
 $_4site = 'not yet';    // valid; starts with an underscore
 $täyte = 'mansikka';    // valid; 'ä' is (Extended) ASCII 228.
 ?>
+
+<?php
+$foo = 'Bob';              // Assign the value 'Bob' to $foo
+$bar = &$foo;              // Reference $foo via $bar.
+$bar = "My name is $bar";  // Alter $bar...
+echo $bar;
+echo $foo;                 // $foo is altered too.
+?>
+
+<?php
+$foo = 25;
+$bar = &$foo;      // This is a valid assignment.
+$bar = &(24 * 7);  // Invalid; references an unnamed expression.
+
+function test()
+{
+   return 25;
+}
+
+$bar = &test();    // Invalid.
+?>
---
+++ php.net/variables/external.php
@@ -0,0 +1,44 @@
+<form action="foo.php" method="post">
+    Name:  <input type="text" name="username" /><br />
+    Email: <input type="text" name="email" /><br />
+    <input type="submit" name="submit" value="Submit me!" />
+</form>
+<?php
+echo $_POST['username'];
+echo $_REQUEST['username'];
+?>
+
+
+<?php
+if ($_POST) {
+    echo '<pre>';
+    echo htmlspecialchars(print_r($_POST, true));
+    echo '</pre>';
+}
+?>
+<form action="" method="post">
+    Name:  <input type="text" name="personal[name]" /><br />
+    Email: <input type="text" name="personal[email]" /><br />
+    Beer: <br />
+    <select multiple name="beer[]">
+        <option value="warthog">Warthog</option>
+        <option value="guinness">Guinness</option>
+        <option value="stuttgarter">Stuttgarter Schwabenbräu</option>
+    </select><br />
+    <input type="submit" value="submit me!" />
+</form>
+
+<?php
+  setcookie("MyCookie[foo]", 'Testing 1', time()+3600);
+  setcookie("MyCookie[bar]", 'Testing 2', time()+3600);
+?>
+
+<?php
+if (isset($_COOKIE['count'])) {
+    $count = $_COOKIE['count'] + 1;
+} else {
+    $count = 1;
+}
+setcookie('count', $count, time()+3600);
+setcookie("Cart[$count]", $item, time()+3600);
+?>
